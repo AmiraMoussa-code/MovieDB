d2857ba768775d73a02774efa20ea53553893db8
AmiraMoussa-code <amira.moussa7197@gmail.com>
2020-10-23 12:12:58 +0300

    step 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,4 +8,14 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/test', (req, res)=>{
+    res.status(200).send({message : 'ok'})
+})
+
+app.get('/time' , (req, res)=>{
+    var myDate = new Date();
+    var time = myDate.getHours() + ":" + myDate.getMinutes();
+    res.status(200).send({message : time});
 })
